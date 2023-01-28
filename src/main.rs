cbebff87f67b77cb7db501ec9d45ca1f640b784a
yinkakun <yinkakun@gmail.com>
2023-01-28 19:00:39 +0100

    if else statements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+
+    let number = 19;
+
+    if number > 10 {
+        println!("number is greater than 10")
+    } else if number == 10 {
+        println!("number is equal to 10")
+    } else {
+        println!("number is less than 10")
+    }
 }
